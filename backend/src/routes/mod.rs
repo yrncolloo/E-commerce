d4ae542f6ec50b107b9b5d802fb751f1e8754905
yrncollo <ngenocollins599@gmail.com>
2024-05-18 17:23:08 +0300

    users can now create their addresses

--- backend/src/routes/mod.rs
+++ backend/src/routes/mod.rs
@@ -3,14 +3,16 @@ mod login;
 mod logout;
 mod items;
 mod reviews;
+mod customer;
 use axum::{middleware, routing::{get, post}, Router};
 use serde::{Deserialize, Serialize};
 
 use crate::utils::{app_state::AppState, custom_middleware::guard_routes};
-use self::{items::list_items::{list_all_items, list_one_item}, login::login, logout::logout, register::register, reviews::{list::{list_all_reviews, list_reviews}, new_review::create_review}};
+use self::{customer::address::create_address, items::list_items::{list_all_items, list_one_item}, login::login, logout::logout, register::register, reviews::{list::{list_all_reviews, list_reviews}, new_review::create_review}};
 
 pub fn create_route(app_state: AppState) -> Router{
     Router::new()
+        .route("/api/v1/address", post(create_address))
         .route("/api/v1/review", post(create_review))
         .route("/api/v1/review/:id", get(list_reviews))
         .route("/api/v1/review", get(list_all_reviews))
