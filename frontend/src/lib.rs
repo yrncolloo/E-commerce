d85c8f370640a1b452e2017b8ee2135757a4e215
yrncollo <ngenocollins599@gmail.com>
2024-06-10 18:54:15 +0300

    changed my mind and deleted everything

--- frontend/src/lib.rs
+++
@@ -1,9 +0,0 @@
-mod routes;
-
-use routes::create_routes;
-
-pub async fn launch_client(){
-    let app = create_routes();
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8081").await.unwrap();
-    axum::serve(listener, app).await.unwrap()
-}
--- frontend/src/main.rs
+++
@@ -1,7 +0,0 @@
-use frontend::launch_client;
-
-
-#[tokio::main]
-async fn main() {
-    launch_client().await;
-}
