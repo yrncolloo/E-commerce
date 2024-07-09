a8d016e125b113d41e930e23f903486c15bda005
yrncollo <ngenocollins599@gmail.com>
2024-07-09 22:13:22 +0300

    mod

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -14,7 +14,7 @@ pub fn create_route(app_state: AppState) -> Router {
     let asset_path = std::env::current_dir().unwrap();
     Router::new()
         .nest("/api/v1", backend_routes(app_state.clone()))
-        .nest("/api/admin/v1", admin_routes(app_state))
-        .nest("/", frontend_routes())
+        .nest("/api/admin/v1", admin_routes(app_state.clone()))
+        .nest("/", frontend_routes(app_state.clone()))
         .nest_service("/assets", ServeDir::new(format!("{}/assets", asset_path.to_str().unwrap())))
 }
